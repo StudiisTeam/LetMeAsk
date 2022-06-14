40db03dab35b284d7d8cb2b5ee7a0a70a06ed51c
MatheusFontenele <matheus.fontenele@icloud.com>
2022-06-14 00:49:05 -0300

    feat: change env to process

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -8,16 +8,16 @@ import { getAuth } from "firebase/auth";
 import 'firebase/database'
 import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
-console.log(import.meta.env.VITE_API_KEY);
+console.log(process.env.VITE_API_KEY);
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_DATABASE_URL,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: process.env.VITE_API_KEY,
+  authDomain: process.env.VITE_AUTH_DOMAIN,
+  databaseURL: process.env.VITE_DATABASE_URL,
+  projectId: process.env.VITE_PROJECT_ID,
+  storageBucket: process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
